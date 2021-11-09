fc7338083b84872b121d20f0ffca463d9bf56baf
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-11-09 19:17:39 +0900

    [#3] test: ArticleFavorite equals, hashcode redefinition test addition

---
+++ src/test/java/com/study/realworld/articlefavorite/domain/ArticleFavoriteTest.java
@@ -0,0 +1,70 @@
+package com.study.realworld.articlefavorite.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import com.study.realworld.article.domain.Article;
+import com.study.realworld.article.domain.ArticleContent;
+import com.study.realworld.article.domain.Body;
+import com.study.realworld.article.domain.Description;
+import com.study.realworld.article.domain.SlugTitle;
+import com.study.realworld.article.domain.Title;
+import com.study.realworld.tag.domain.Tag;
+import com.study.realworld.user.domain.Bio;
+import com.study.realworld.user.domain.Email;
+import com.study.realworld.user.domain.Follows;
+import com.study.realworld.user.domain.Password;
+import com.study.realworld.user.domain.User;
+import com.study.realworld.user.domain.Username;
+import java.util.Arrays;
+import java.util.HashSet;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ArticleFavoriteTest {
+
+    private User user;
+    private Article article;
+
+    @BeforeEach
+    void beforeEach() {
+        user = User.Builder()
+            .profile(Username.of("jake"), Bio.of("I work at statefarm"), null)
+            .email(Email.of("jake@jake.jake"))
+            .password(Password.of("jakejake"))
+            .follows(Follows.of(new HashSet<>()))
+            .build();
+
+        ArticleContent articleContent = ArticleContent.builder()
+            .slugTitle(SlugTitle.of(Title.of("How to train your dragon")))
+            .description(Description.of("Ever wonder how?"))
+            .body(Body.of("It takes a Jacobian"))
+            .tags(Arrays.asList(Tag.of("dragons"), Tag.of("training")))
+            .build();
+
+        article = Article.from(articleContent, user);
+    }
+
+    @Test
+    void articleFavoriteTest() {
+        ArticleFavorite articleFavorite = new ArticleFavorite();
+    }
+
+    @Test
+    @DisplayName("equals hashCode 테스트")
+    void articleFavoriteEqualsHashCodeTest() {
+
+        // when
+        ArticleFavorite result = ArticleFavorite.from(user, article);
+
+        // then
+        assertThat(result)
+            .isEqualTo(ArticleFavorite.from(user, article))
+            .hasSameHashCodeAs(ArticleFavorite.from(user, article));
+        assertEquals(result, result);
+        assertNotEquals(result, null);
+    }
+
+}
